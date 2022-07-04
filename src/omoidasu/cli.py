0d8ed233c1fa1578cc0b90f1951accfa1d3cff60
0djentd <58089540+0djentd@users.noreply.github.com>
2022-07-05 01:40:18 +0300

    modified:   src/omoidasu/cli.py

--- src/omoidasu/cli.py
+++ src/omoidasu/cli.py
@@ -1,46 +1,42 @@
+"""CLI module."""
+
+
 import click
 
 
 @click.group()
 def main():
     """Main function."""
-    pass
 
 
 @main.command()
 def list_cards():
     """List all cards."""
-    pass
 
 
 @main.command()
 def review_cards():
     """Review all cards."""
-    pass
 
 
 @main.command()
 def add_card():
     """Add new card."""
-    pass
 
 
 @main.command()
 def remove_card():
     """Remove card."""
-    pass
 
 
 @main.command()
 def edit_card():
     """Edit card."""
-    pass
 
 
 @main.command()
 def stats():
     """Show user stats."""
-    pass
 
 
 if __name__ == "__main__":
