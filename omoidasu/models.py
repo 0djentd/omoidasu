4a573ba2340a2011e734cf2d571d51532c8c9513
0djentd <58089540+0djentd@users.noreply.github.com>
2022-07-31 15:51:28 +0300

    modified:   omoidasu/models.py

--- omoidasu/models.py
+++ omoidasu/models.py
@@ -1,5 +1,7 @@
 import logging
+from typing import Optional
 
+import rich
 from pydantic import BaseModel
 
 from omoidasu.exceptions import NotEnoughCardSidesException
@@ -9,13 +11,13 @@ logger = logging.getLogger(__name__)
 
 class Side(BaseModel):
     """Card side model."""
-    id: int
+    id: int  # Line number.
     content: str
 
 
 class Card(BaseModel):
     """Card model."""
-    filename: str
+    filename: Optional[str]  # Can be None, if not saved to file.
     sides: list[Side]
 
     @classmethod
@@ -32,3 +34,15 @@ class Card(BaseModel):
         card = cls(filename=filename, sides=sides)
         logger.info("Loaded %s from %s", card, filename)
         return card
+
+    def review(self):
+        """Review all combinations."""
+        sides = self.sides
+        for question in sides:
+            for answer in sides:
+                _ = input(question)
+                result = input(answer)
+                if result not in ["", "y", "Y", "\n"]:
+                    rich.print("[red]Wrong.[/red]")
+                else:
+                    rich.print("[green]Correct.[/green]")
