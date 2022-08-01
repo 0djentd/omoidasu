2c2038dde01da64a640f5bb0873c7a29b5c8977d
0djentd <58089540+0djentd@users.noreply.github.com>
2022-08-01 18:47:53 +0300

    new file:   tests/test_models.py

---
+++ tests/test_models.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+from secrets import token_urlsafe
+from typing import Iterable
+
+from omoidasu.models import Card, Side, Question
+from omoidasu.exceptions import NotEnoughCardSidesException
+
+
+def generate_card_sides(count):
+    result = []
+    for i in range(count):
+        side = Side(id=i, content=token_urlsafe())
+        result.append(side)
+    return result
+
+class ModelsTests(TestCase):
+    def test_card_constructor(self):
+        sides = generate_card_sides(2)
+        card = Card(filename=None, sides=sides)
+        self.assertEqual(card.filename, None)
+
+    def test_card_constructor_exception(self):
+        with self.assertRaises(NotEnoughCardSidesException):
+            Card(filename=None, sides=generate_card_sides(1))
